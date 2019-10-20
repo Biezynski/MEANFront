c06ed7975bef373b6001d4bc277397222063b7c2
biezynski <wojciech.biezynski@onet.pl>
2019-10-20 21:07:18 +0200

    commit u Kasi

--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Post } from '../../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -8,10 +10,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class PostCreateComponent implements OnInit {
   postHeader = '';
   postContent = '';
-  @Output() outPosts = new EventEmitter();
+  @Output() outPosts = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post = { title: this.postHeader, content: this.postContent };
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = { title: form.value.title, content: form.value.content };
     this.outPosts.emit(post);
   }
 
