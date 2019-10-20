ffde105023affbe87ad66d40b218834cd858bd4e
biezynski <wojciech.biezynski@onet.pl>
2019-10-20 18:35:02 +0200

    dodany event emitter przez post create component do app component i z app component do post list component

--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -8,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class PostCreateComponent implements OnInit {
   postHeader = '';
   postContent = '';
+  @Output() outPosts = new EventEmitter();
 
   onAddPost() {
     const post = { title: this.postHeader, content: this.postContent };
-
+    this.outPosts.emit(post);
   }
 
   constructor() { }
--- src/app/posts/post-list/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -12,8 +12,7 @@ export class PostListComponent implements OnInit {
   //   { title: 'to jest trzeci tytuł', content: 'to jest zawartość trzeciego posta' },
   // ];
 
-  posts = [];
-
+  @Input() posts = [];
 
   constructor() { }
 
