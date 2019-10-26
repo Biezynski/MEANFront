36c972a39b1c49688882fe5c2904d581fc39642f
Wojciech Biezynski <wojciech.biezynski@onet.pl>
2019-10-26 20:42:02 +0200

    some back

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const app = require('./backend/app');
 
-const server = http.createServer((req, res) => {
-    res.end('zakonczenie servera');
-
-})
-server.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000
+app.set('port', port);
+const server = http.createServer(app)
+server.listen(port);
