8a72a9ac0ac81ec52a9f7844b70c9d3800b33660
Wojciech Biezynski <wojciech.biezynski@onet.pl>
2019-11-16 20:55:01 +0100

    some endpoints added, refactor backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Post = require('./models/post');
 const app = express();
 const mongoose = require('mongoose');
-
+const postsRoutes = require('./routes/posts');
 
 mongoose.connect('mongodb+srv://Wojtek:E7FpPnrRCER8dXCl@cluster0-n9gde.mongodb.net/node-angular?retryWrites=true&w=majority')
     .then(() => {
@@ -19,39 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 })
-
-
-app.post('/api/posts', (req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Poprawnie dodano Posta',
-            postId: createdPost._id
-        });
-    });
-});
-
-app.get('/api/posts', (req, res, next) => {
-    Post.find()
-        .then((documents) => {
-            res.status(200).json({
-                message: 'Poprawnie pobrano posty!',
-                posts: documents
-            });
-        })
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-        console.log(result);
-        res.status(200).json({ message: "Post deleted!" });
-    });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Post } from '../../post.model';
 import { NgForm } from '@angular/forms';
 import { PostsService } from '../../posts.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-post-create',
@@ -11,18 +12,43 @@ import { PostsService } from '../../posts.service';
 export class PostCreateComponent implements OnInit {
   postHeader = '';
   postContent = '';
+  private mode = 'create';
+  private postId: string;
+  post: Post;
 
-
-  constructor(public postService: PostsService) { }
+  constructor(
+    public postService: PostsService,
+    public route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postService.getPost(this.postId).subscribe(postData => {
+          this.post = { id: postData._id, title: postData.title, content: postData.content }
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+
+    });
   }
 
-  onAddPost(form: NgForm) {
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postService.updatePost(this.postId, form.value.title, form.value.content);
+    }
+    form.resetForm();
   }
 
 
